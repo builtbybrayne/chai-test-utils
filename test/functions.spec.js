d441cc6e5a8716d1ed69d6ffab31ceb68ed39526
Alastair Brayne <al@builtbybrayne.com>
2017-05-31 15:04:45 +0100

    Add some tests for index export

---
+++ test/functions.spec.js
@@ -0,0 +1,34 @@
+'use strict';
+import 'babel-polyfill';
+const chai = require('chai');
+const expect = chai.expect;
+chai.use(require('chai-as-promised'));
+
+const {asyncTest, asyncThrows} = require('../src/functions');
+
+describe('functions', function(){
+  it('should correctly run async tests', asyncTest(async () => {
+    let result = false;
+    const fn = async () => {
+      result = true
+    };
+    await fn();
+    expect(result).to.be.true;
+  }));
+
+  it('should correctly throw asyncThrows', asyncTest(async () => {
+    const error = new Error();
+    const result = await asyncThrows(async () => {throw error;});
+    expect(result).to.eql(error);
+
+    expect(async () => await asyncThrows(async () => {})).to.reject;
+  }));
+
+  it('should correctly not throw asyncThrows', asyncTest(async () => {
+    const result = await asyncThrows(async () => {}, true);
+    expect(result).not.to.exist;
+
+    expect(async () => await asyncThrows(async () => {})).to.resolve;
+  }));
+  
+});
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,34 +1,21 @@
 'use strict';
 import 'babel-polyfill';
-const chai = require("chai");
+const chai = require('chai');
 const expect = chai.expect;
 chai.use(require('chai-as-promised'));
 
-const {asyncTest, asyncThrows} = require("../src/index");
+import * as Index from '../src/index';
 
-describe('Async', function(){
-  it('should correctly run async tests', asyncTest(async () => {
-    let result = false;
-    const fn = async () => {
-      result = true
-    };
-    await fn();
-    expect(result).to.be.true;
-  }));
+describe('Index', function(){
+  it('should expose the functions', () => {
+    expect(Index.asyncThrows).to.be.a('function');
+    expect(Index.resolveAndTick).to.be.a('function');
+    expect(Index.asyncTest).to.be.a('function');
+  });
 
-  it('should correctly throw asyncThrows', asyncTest(async () => {
-    const error = new Error();
-    const result = await asyncThrows(async () => {throw error;});
-    expect(result).to.eql(error);
-
-    expect(async () => await asyncThrows(async () => {})).to.reject;
-  }));
-
-  it('should correctly not throw asyncThrows', asyncTest(async () => {
-    const result = await asyncThrows(async () => {}, true);
-    expect(result).not.to.exist;
-
-    expect(async () => await asyncThrows(async () => {})).to.resolve;
-  }));
+  it('exposes the Test object', () => {
+    expect(Index.Tests).to.exist;
+    expect(Index.Tests.goodAndBad).to.be.a('function');
+  });
   
 });
